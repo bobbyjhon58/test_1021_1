5b3a14ff54d3dfe8e0bc579d9c86f69bde9826a2
g <g@DESKTOP-9604JOP>
2020-10-26 15:28:11 +0900

    비동기식 로그인과 로그아웃

---
+++ MyProject/src/main/java/mybatis/vo/MemVO.java
@@ -0,0 +1,30 @@
+package mybatis.vo;
+
+public class MemVO {
+	private String m_id, m_pw, name;
+
+	public String getM_id() {
+		return m_id;
+	}
+
+	public void setM_id(String m_id) {
+		this.m_id = m_id;
+	}
+
+	public String getM_pw() {
+		return m_pw;
+	}
+
+	public void setM_pw(String m_pw) {
+		this.m_pw = m_pw;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
